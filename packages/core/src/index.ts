a7953a12d7442a77851e076d78ac0280eac3b0c4
Michael Arnaldi <arnaldimichael@gmail.com>
2021-09-30 13:35:48 +0100

    chore: add fluent example

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 import { $Option } from "@effect-ts/system"
 
 console.log($Option.some(0).pipe($Option.flatMap((a) => $Option.some(a))))
+console.log($Option.some(0).flatMap((a) => $Option.some(a)))

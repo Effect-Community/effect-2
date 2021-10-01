aea0a1c6b17ac2a25b6c3e292452a98bc5174c99
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-01 19:42:33 +0100

    chore: improve ts structure

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,13 @@
-import { $Option } from "@effect-ts/system"
+import { $Effect, $Option } from "@effect-ts/system"
 
 console.log($Option.some(0).pipe($Option.flatMap((a) => $Option.some(a))))
 console.log($Option.some(0).flatMap((a) => $Option.some(a)))
+
+console.log(
+  $Effect.do
+    .bind("x", () => $Effect.succeed(() => 1))
+    .bind("y", () => $Effect.succeed(() => 2))
+    .bind("z", ({ x, y }) => $Effect.succeed(() => x + y))
+    .map(({ z }) => z)
+    .forever()
+)
---
+++ packages/system/src/modules/Effect/delay.ts
@@ -0,0 +1,31 @@
+import type { $EffectOps, $EffectStaticOps } from "./type"
+import { registerEffectOp, registerEffectStaticOp } from "./type"
+
+declare module "./type" {
+  interface $EffectOps {
+    /**
+     * @ets_method delay_ from "@effect-ts/system/modules/Effect/delay"
+     */
+    delay<R, E, A>(this: $Effect<R, E, A>, ms: number): $Effect<R, E, never>
+  }
+  interface $EffectStaticOps {
+    /**
+     * @ets_static delay from "@effect-ts/system/modules/Effect/delay"
+     * @ets_unpipe delay_
+     */
+    delay(ms: number): <R, E, A>(self: $Effect<R, E, A>) => $Effect<R, E, never>
+  }
+}
+
+export const delay_: $EffectOps["delay"] = function (_ms) {
+  throw new Error("NotImplemented")
+}
+
+export const delay: $EffectStaticOps["delay"] = function (ms) {
+  return (self) => self.delay(ms)
+}
+
+if (typeof ETS_PLUGIN === "undefined" || !ETS_PLUGIN) {
+  registerEffectOp("delay")(delay_)
+  registerEffectStaticOp("delay")(delay)
+}

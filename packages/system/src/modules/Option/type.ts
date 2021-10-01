9239a8db652a7c01399214f99bbb439377366ef2
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-01 18:38:40 +0100

    chore: add missing PURE

--- packages/system/src/modules/Option/type.ts
+++ packages/system/src/modules/Option/type.ts
@@ -15,12 +15,13 @@ export interface $OptionOps {}
 export interface $OptionStaticOps {}
 export const $Option = {} as $OptionStaticOps
 
-export const registerOptionOp = polyfiller<$OptionOps>([
-  ISome.prototype,
-  INone.prototype
-])
+export const registerOptionOp =
+  /* #__PURE__ */
+  polyfiller<$OptionOps>([ISome.prototype, INone.prototype])
 
-export const registerOptionStaticOp = polyfiller<$OptionStaticOps>([$Option])
+export const registerOptionStaticOp =
+  /* #__PURE__ */
+  polyfiller<$OptionStaticOps>([$Option])
 
 export namespace $Option {
   export type _OutOf<T> = [T] extends [

0e382012a00ba6eabb7073a5f65e32a7a633b27f
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-02 17:02:07 +0100

    chore: consolidate pattern

--- packages/system/src/modules/Effect/type.ts
+++ packages/system/src/modules/Effect/type.ts
@@ -16,14 +16,7 @@ export declare const _E: unique symbol
 export declare const _A: unique symbol
 export declare const _EffectId: unique symbol
 
-export interface $Effect<R, E, A> extends $EffectOps {
-  readonly [_Internal]: {
-    readonly [_EffectId]: typeof _EffectId
-    readonly [_R]: (_R: R) => void
-    readonly [_E]: () => E
-    readonly [_A]: () => A
-  }
-}
+export type $Effect<R, E, A> = $Effect.Effect<R, E, A>
 
 export interface $EffectStaticOps {}
 export const $Effect = {} as $EffectStaticOps
@@ -48,10 +41,18 @@ export const registerEffectStaticOp =
   polyfiller<$EffectStaticOps>($Effect)
 
 export declare namespace $Effect {
-  export type Effect<R, E, A> = $Effect<R, E, A>
   export type IO<A> = $Effect<unknown, never, A>
   export type RIO<R, A> = $Effect<R, never, A>
   export type EIO<E, A> = $Effect<unknown, E, A>
+
+  export interface Effect<R, E, A> extends $EffectOps {
+    readonly [_Internal]: {
+      readonly [_EffectId]: typeof _EffectId
+      readonly [_R]: (_R: R) => void
+      readonly [_E]: () => E
+      readonly [_A]: () => A
+    }
+  }
 
   export type _R<T> = [T] extends [
     {
